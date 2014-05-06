62948f6ee06b1e8dee7e42e34b680fb320a0a3fb
ahuber1 <andrewhuber93@icloud.com>
2014-05-06 10:50:02 -0400

    Brought in accelerometer capabilities to Trooper.java. Need to alter Trooper's constructor calls because I altered the constructor a bit. Need to now calculate the percentage of tilt and make trooper move left and right.

--- src/com/falling/object/Trooper.java
+++ src/com/falling/object/Trooper.java
@@ -1,13 +1,23 @@
 package com.falling.object;
 
+import java.util.Date;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 /**
  * This manages information regarding the paratrooper, the hero of our game!
  * @author evan hanger, andrew huber, mark judy
  * @version 1.0 Date: 5/2/14 Time: 11:15 PM
  */
 public class Trooper extends GameObject
-{
-    public Trooper()
+{	
+	private SensorManager manager;
+	
+	public Trooper(Context context)
     {
         super();
         alive = true;
@@ -15,6 +25,10 @@ public class Trooper extends GameObject
         centerY = 200;
         speedY = 0;
         speedX = 0;
+        manager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+		manager.registerListener(listener, 
+				manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 
+				SensorManager.SENSOR_DELAY_GAME);
     }
 
     public void update()
@@ -43,4 +57,34 @@ public class Trooper extends GameObject
 
         //TODO handle collisions
     }
+    
+    
+private SensorEventListener listener = new SensorEventListener() {
+		
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			double accelx = event.values[0];
+			double accely = event.values[1];
+			double accelz = event.values[2];
+			
+			double gravx = 0;
+			double gravy = accelz > accely ? 0 : 9.8;
+			double gravz = accelz > accely ? 9.8 : 0;
+			
+			double accelmag = Math.sqrt(Math.pow(accelx, 2) + Math.pow(accely, 2) + Math.pow(accelz, 2));
+			double gravmag = 9.8;
+			
+			double num = accelx * gravx + accely * gravy + accelz * gravz;
+			double den = accelmag * gravmag;
+			
+			double angle = Math.toDegrees(Math.acos(num / den));
+			boolean left = angle > 0;
+			double percentage = Math.abs(angle) / 90.0;
+		}
+		
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// Do nothing...
+		}
+	};
 }
