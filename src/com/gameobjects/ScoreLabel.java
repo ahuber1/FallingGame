ebd5eb5099392a297da19734f824bbeade8d72e9
mjudy <mjudy1@umbc.edu>
2014-05-13 23:24:16 -0400

    comments

--- src/com/gameobjects/ScoreLabel.java
+++ src/com/gameobjects/ScoreLabel.java
@@ -5,11 +5,20 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 
+/**
+ * Defines a game object that shows the current score to the player.
+ */
 public class ScoreLabel extends GameObject{
 	
 	public int score;
 	public int gameHeight;
 
+    /**
+     * Creates the score label at the given x and y coordinates, with the game height for scaling the text size.
+     * @param x the top left x coordinate
+     * @param y the top left y coordinate
+     * @param gameHeight The height of the game area for scaling the text size.
+     */
 	public ScoreLabel(float x, float y, int gameHeight){
 		
 		this.x = x;
@@ -17,12 +26,21 @@ public class ScoreLabel extends GameObject{
 		this.score = 0;
 		this.gameHeight = gameHeight;
 	}
-	
+
+    /**
+     * Sets the label to show a new score.
+     * @param score the new score to show
+     */
 	public void setScore(int score){
 		
 		this.score = score;
 	}
-	
+
+    /**
+     * Draws the score label on the given canvas
+     * @param canvas the canvas on which to draw the sprite
+     */
+    @Override
 	public void draw(Canvas canvas){
 		
 		String text = String.format("Score: %d", score);
