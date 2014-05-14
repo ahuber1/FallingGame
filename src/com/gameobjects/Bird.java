985f03f88335166c9bc9fa34f62e65b43cf19678
mjudy <mjudy1@umbc.edu>
2014-05-13 23:22:00 -0400

    comments

--- src/com/gameobjects/Bird.java
+++ src/com/gameobjects/Bird.java
@@ -7,10 +7,21 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+/**
+ * Defines the bird game object for the falling game.
+ */
 public class Bird extends GameObject{
 
 	private Bitmap spriteL, spriteR;
-	
+
+    /**
+     * Constructs a new bird game object.
+     * @param x the starting x coordinate
+     * @param y the starting y coordinate
+     * @param dx the velocity on the x axis
+     * @param dy the velocity on the y axis
+     * @param context the application environment of this object
+     */
 	public Bird(float x, float y, float dx, float dy, Context context){
 		
 		this.alive = true;
