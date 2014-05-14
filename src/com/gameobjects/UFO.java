a9f90989b87728beab4e86a2fb7431b65e598ac8
mjudy <mjudy1@umbc.edu>
2014-05-13 23:25:17 -0400

    comments

--- src/com/gameobjects/UFO.java
+++ src/com/gameobjects/UFO.java
@@ -5,8 +5,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import com.example.fallinggametest.R;
 
+/**
+ * Defines the UFO game object for Falling.
+ */
 public class UFO extends GameObject {
 
+    /**
+     * Constructs a new UFO game object.
+     * @param x the starting x coordinate
+     * @param y the starting y coordinate
+     * @param dx the velocity on the x axis
+     * @param dy the velocity on the y axis
+     * @param context the application environment of this object
+     */
     public UFO(float x, float y, float dx, float dy, Context context)
     {
 
