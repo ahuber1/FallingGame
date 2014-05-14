c56adffbd8ff0ed064a495952b80216c6f08971a
mjudy <mjudy1@umbc.edu>
2014-05-13 23:38:43 -0400

    fixed!

--- src/com/gameobjects/Plane.java
+++ src/com/gameobjects/Plane.java
@@ -11,45 +11,14 @@ import android.graphics.BitmapFactory;
  */
 public class Plane extends GameObject{
 	
-	Bitmap spriteL, spriteR;
-
-	package com.gameobjects;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import com.example.fallinggametest.R;
-
-/**
- * Defines the UFO game object for Falling.
- */
-public class UFO extends GameObject {
-
-    /**
-     * Constructs a new airplane game object.
-     * @param x the starting x coordinate
-     * @param y the starting y coordinate
-     * @param dx the velocity on the x axis
-     * @param dy the velocity on the y axis
-     * @param context the application environment of this object
-     */
-    public UFO(float x, float y, float dx, float dy, Context context)
-    {
-
-    	this.alive = true;
-    	
-        this.x = x;
-        this.y = y;
-
-        this.dx = dx;
-        this.dy = dy;
-
-        this.sprite = BitmapFactory.decodeResource(context.getResources(), R.drawable.ufo);
-   
-        createHitboxForSprite();
-    }
-
-}
+	/**
+	 * Constructs a new airplane game object.
+         * @param x the starting x coordinate
+         * @param y the starting y coordinate
+         * @param dx the velocity on the x axis
+         * @param dy the velocity on the y axis
+         * @param context the application environment of this object
+         */
 	public Plane(float x, float y, float dx, float dy, Context context){
 		
 		this.x = x;
